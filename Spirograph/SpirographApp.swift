a864f7ade5fa19c007437bbb5ebe29c47932f78e
Takurus <info@takurus.com>
2022-07-20 06:59:46 -0400

    Initial Commit

---
+++ Spirograph/SpirographApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpirographApp.swift
+//  Spirograph
+//
+//  Created by Jack Farray on 2022-07-20.
+//
+
+import SwiftUI
+
+@main
+struct SpirographApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
